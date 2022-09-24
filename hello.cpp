13e50068a6c786f8421d04a501ab080c44fe7f8c
ScriptErrorVGM <mixajlov.grigory@gmail.com>
2022-09-24 15:56:49 +1000

    normal push

---
+++ hello.cpp
@@ -0,0 +1,20 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << " Hello";
+    cout << " Hello";
+    cout << " Hello";
+    /*vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+    for (const string& word : msg)
+    {
+        cout << word << " Hello";
+    }
+    cout << endl;*/
+    return 0;
+}
