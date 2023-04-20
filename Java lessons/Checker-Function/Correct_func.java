5640e428cb3e79097ff9d0f4faa29a1baab58293
ScriptErrorVGM <mixajlov.grigory@gmail.com>
2023-04-20 23:20:15 +1000

    20/04/2023 Checker upload "in progress"

---
+++ Java lessons/Checker-Function/Correct_func.java
@@ -0,0 +1,17 @@
+package Java lessons.Checker-Function;
+
+import java.util.regex.*;
+public class Correct_func {
+    
+    static boolean isCorrect(String str){
+        
+        String regex =("[А-Яа-я]*\\s[А-Яа-я]*\\s[А-Яа-я]*(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\\d\\d*\\s");
+        Pattern patrn = Pattern.compile(regex);
+
+        String[] ss = str.split(",");
+        for (String s: ss){
+            
+        }
+    }
+    
+}
