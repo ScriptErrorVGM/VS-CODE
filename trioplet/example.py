29b39318bfa6a5bb4d4c38638538903180d11a4f
ScriptErrorVGM <mixajlov.grigory@gmail.com>
2023-10-13 19:56:06 +1000

    update 2

--- trioplet/example.py
+++ trioplet/example.py
@@ -1,4 +1,9 @@
-a, b = int(input()), int(input())
-for i in range(a, (b + 1)):
-    if str(i)[:2] == str(i)[:1:-1]:
-        print(i)
+x = int(input())
+p = int(input())
+y = int(input())
+i = 0
+while x < y:
+    x *= 1 + p / 100
+    x = int(100 * x) / 100
+    i += 1
+print(i)
